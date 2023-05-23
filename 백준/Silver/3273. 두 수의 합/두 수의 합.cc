ea8b7b58b7022ce0cd351a573c6254ab2ee40efc
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2023-05-23 22:42:39 +0900

    두 수의 합.cc

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 
-	int arr[100000];
-	int check[1000001];
+	int arr[100000]; // 전역으로 선언하지 않으면 스택메모리에 의해 오버플로우가 난다.
+	int check[1000001]; // 1000000도 포함되게 1000001의 크기로 선언
 
 int main(void) {
 	ios::sync_with_stdio(0);
@@ -22,7 +22,7 @@ int main(void) {
 	cin >> x;
 
 	for (int i = 0; i < n; i++) {
-		if (x - arr[i] <= 1000000 && x - arr[i] >= 0) {
+		if (x - arr[i] <= 1000000 && x - arr[i] >= 0) { // (x - arr[i])가 check[]의 배열 길이에 따라 범위 설정을 해줘야 한다.
 		    if (check[x - arr[i]] == 1) cnt++;
 		}
 		check[arr[i]]++;
