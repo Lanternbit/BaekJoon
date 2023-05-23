02d7c4b2d5893f7c140ab7a89a6de16d530075ed
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2023-05-23 21:24:22 +0900

    알파벳 개수.cc

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -45,3 +45,26 @@ int main(void) {
 	    cout << arr[i] << " ";
 	}
 }
+
+/*
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int arr[26] = { 0 };
+	string s;
+
+	cin >> s;
+
+	for (auto c : s) {
+		arr[c - 'a']++;
+	}
+	for (int i = 0; i < 26; i++) {
+		cout << arr[i] << " ";
+	}
+}
+*/
