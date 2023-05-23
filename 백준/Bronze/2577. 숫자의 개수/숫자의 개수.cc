fba913b0dee42056d2c10a292ffc8114b6092729
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2023-05-23 21:42:46 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int arr[10] = { 0 };
+	int A, B, C;
+
+	cin >> A >> B >> C;
+
+	int num = A * B * C;
+
+	for (int i = 0; i < 9; i++) {
+		if (num / 10 == 0 && num % 10 == 0) break;
+		else {
+			arr[num % 10]++;
+			num /= 10;
+		}
+	}
+
+	for (int i = 0; i < 10; i++) cout << arr[i] << "\n";
+}
