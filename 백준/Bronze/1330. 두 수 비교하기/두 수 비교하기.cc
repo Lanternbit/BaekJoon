b2208b472a6e818a7c345d11d13b219795508517
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2023-05-29 23:08:42 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int A, B;
+
+	cin >> A >> B;
+
+	if (A > B) cout << ">";
+	else if (A < B) cout << "<";
+	else cout << "==";
+}
