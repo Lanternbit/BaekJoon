75921a46098bcbc89134c03a9bbcca3a18da1eb2
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2023-12-12 09:31:23 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s;
+	int n;
+
+	cin >> s >> n;
+
+	cout << s[n-1] << "\n";
+}
