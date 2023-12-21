1be2b165077535a8f1fe16b54a0971998f9dc952
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2023-12-21 13:10:40 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<utility>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	vector<pair<int, int>> v;
+
+	cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		int a, b;
+
+		cin >> b >> a;
+
+		v.push_back(pair<int, int>(a, b));
+	}
+
+	sort(v.begin(), v.end());
+
+	vector<pair<int, int>>::iterator iter;
+
+	for (iter = v.begin(); iter < v.end(); iter++) {
+		cout << iter->second << " " << iter->first << "\n";
+	}
+}
