c54ad832869b4b7bc33b77847f32130f96415340
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2023-12-27 22:09:41 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int x, y;
+
+	cin >> x >> y;
+
+	if (x > 0 && y > 0) cout << "1" << "\n";
+	else if (x < 0 && y > 0) cout << "2" << "\n";
+	else if (x < 0 && y < 0) cout << "3" << "\n";
+	else cout << "4" << "\n";
+}
