e40980c883b253131fef6fe8a9dff5a76a07a102
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2023-12-29 07:51:04 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+
+	cin >> n;
+
+	int p = 2;
+
+	for (int i = 0; i < n; i++) {
+		p += (p - 1);
+	}
+
+	cout << p * p << "\n";
+}
