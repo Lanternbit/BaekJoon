204896215c80a035e6a5b2a37f3ec71359ad0391
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2023-12-28 15:54:30 +0900

    크로아티아 알파벳.cc

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -2,55 +2,26 @@
 #include<string>
 using namespace std;
 
-// - -> 45
-// = -> 61
-// j -> 106
-
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	string word;
-	int ans = 0;
 
 	cin >> word;
 
-	for (int i = 0; i < word.length(); i++) {
-		int c = int(word[word.length() - i - 1]);
+	string arr[8] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+	int idx;
 
-		if (c == 45) {
-			ans++;
-			i++;
+	for (int i = 0; i < 8; i++) {
+		while (true) {
+			idx = word.find(arr[i]);
+
+			if (idx == string::npos) break;
+
+			word.replace(idx, arr[i].length(), "a");
 		}
-		else if (c == 61) {
-			switch (int(word[word.length() - i - 2])) {
-				case 99:
-				case 115:
-					ans++;
-					i++;
-					break;
-				case 122:
-					if (int(word[word.length() - i - 3]) == 100) {
-						ans++;
-						i++;
-						i++;
-					}
-					else {
-						ans++;
-						i++;
-					}
-					break;
-			}
-		}
-		else if (c == 106) {
-			if (int(word[word.length() - i - 2]) == 108 || int(word[word.length() - i - 2]) == 110) {
-				ans++;
-				i++;
-			}
-			else ans++;
-		}
-		else ans++;
 	}
 
-	cout << ans << "\n";
+	cout << word.length() << "\n";
 }
