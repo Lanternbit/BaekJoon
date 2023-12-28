99d5936e5ec437ab7c2ec698c46f420a3751d5bd
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2023-12-28 12:01:23 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string word;
+
+	cin >> word;
+
+	cout << word.length() << "\n";
+}
