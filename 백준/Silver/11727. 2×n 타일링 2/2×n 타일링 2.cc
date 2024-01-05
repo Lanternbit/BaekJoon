5ed3a2b9eccccc750fecb9f70f4b75e1f073fe96
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2024-01-05 23:13:22 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	vector<int> v;
+
+	cin >> n;
+
+	v.push_back(1);
+	v.push_back(1);
+
+	for (int i = 2; i <= n; i++) {
+		v.push_back((v[i - 1] + (2 * v[i - 2])) % 10007);
+	}
+
+	cout << v[n] << "\n";
+}
