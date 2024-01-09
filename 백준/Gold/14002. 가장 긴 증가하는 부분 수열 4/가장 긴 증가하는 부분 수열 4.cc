6e7da12316449180edf85c0b4d42e90bdd8e00f5
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2024-01-09 09:39:38 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	vector<vector<int>> v;
+	vector<int> v1;
+	vector<int> v2;
+	stack<int> s;
+
+	cin >> n;
+
+	v.push_back(v1);
+	v.push_back(v2);
+	v[0].push_back(0);
+	v[1].push_back(0);
+
+	for (int i = 1; i <= n; i++) {
+		int a;
+
+		cin >> a;
+
+		v[0].push_back(a);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		int max = 0;
+
+		for (int j = i - 1; j >= 0; j--) {
+			if (v[0][i] > v[0][j]) {
+				if (max < v[1][j]) max = v[1][j];
+			}
+		}
+
+		v[1].push_back(max + 1);
+	}
+
+	int max = 0;
+
+	for (int i = 0; i < v[1].size(); i++) {
+		if (max < v[1][i]) max = v[1][i];
+	}
+
+	cout << max << "\n";
+
+	for (int i = v[1].size() - 1; i >= 1; i--) {
+		if (v[1][i] == max) {
+			s.push(v[0][i]);
+			max--;
+		}
+	}
+
+	while (!s.empty()) {
+		cout << s.top() << " ";
+		s.pop();
+	}
+}
