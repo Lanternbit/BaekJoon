b3048678ba70337d14e285189f1719a66b4c01f5
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2024-01-24 23:48:54 +0900

    [Silver V] Title: 날짜 계산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a[7981][3];
+	int ans;
+	int e, s, m;
+
+	cin >> e >> s >> m;
+
+	for (int i = 1; i < 7981; i++) {
+		ans = i;
+
+		if (i % 15 == e % 15 && i % 28 == s % 28 && i % 19 == m % 19) break;
+	}
+
+	cout << ans << "\n";
+}
