b55af9ef5af0e2c9dd59f88b3e7a9f81f93ba6bd
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2024-02-02 14:57:08 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n, m;
+vector<int> v;
+int arr[8];
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if(k == 0) arr[k] = v[i];
+		else {
+			if (arr[k - 1] <= v[i]) arr[k] = v[i];
+			else continue;
+		}
+
+		func(k + 1);
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+
+	v.push_back(0);
+
+	for (int i = 0; i < n; i++) {
+		int a;
+
+		cin >> a;
+
+		v.push_back(a);
+	}
+
+	sort(v.begin(), v.end());
+	func(0);
+
+	return 0;
+}
