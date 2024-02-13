1a358f09bbeecd5aa1f0a49f7a32fc3af06d6721
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2024-02-13 23:28:12 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+    
+    cout << "     /~\\" << "\n";
+    cout << "    ( oo|" << "\n";
+    cout << "    _\\=/_" << "\n";
+    cout << "   /  _  \\" << "\n";
+    cout << "  //|/.\\|\\\\" << "\n";
+    cout << " ||  \\ /  ||" << "\n";
+    cout << "============" << "\n";
+    cout << "|          |" << "\n";
+    cout << "|          |" << "\n";
+    cout << "|          |" << "\n";
+
+	return 0;
+}
