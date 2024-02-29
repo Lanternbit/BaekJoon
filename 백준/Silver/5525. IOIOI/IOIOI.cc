8d9b8fe59a3b350b805870e2d28a74ad03458fce
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2024-02-29 12:06:20 +0900

    IOIOI.cc

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -7,13 +7,10 @@ string s;
 string p;
 int ans = 0;
 int len = 0;
-int check = 0;
 
 void matching() {
 	for (int i = 0; i < s.length(); i++) {
 		if (s[i] == 'I') {
-			check = 1;
-
 			while (s[i + 1] == 'O' && s[i + 2] == 'I') {
 				len++;
 				i += 2;
